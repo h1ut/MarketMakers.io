25a6381b4e5fffa223c3e40116fa7aa1e9a342ae
h1ut <amirosmonalievgg@gmail.com>
2025-12-30 10:18:52 +0600

    Fix Render build errors

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   // Для GitHub Pages: https://h1ut.github.io/MarketMakers.io/
-  //base: process.env.NODE_ENV === "production" ? "/MarketMakers.io/" : "/",
-  base: "https://h1ut.github.io/MarketMakers.io/",
+  // base должен быть ОТНОСИТЕЛЬНЫМ путем, а не полным URL!
+  base: process.env.NODE_ENV === "production" ? "/MarketMakers.io/" : "/",
   server: {
     port: 5173
   }
