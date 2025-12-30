df74005a5b798ea4d172c40009bf3eefbcfcd795
h1ut <amirosmonalievgg@gmail.com>
2025-12-30 10:12:28 +0600

    Configure GitHub Pages deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/MarketMakers.io",
+  // Для GitHub Pages: https://h1ut.github.io/MarketMakers.io/
+  //base: process.env.NODE_ENV === "production" ? "/MarketMakers.io/" : "/",
+  base: "https://h1ut.github.io/MarketMakers.io/",
   server: {
     port: 5173
   }
